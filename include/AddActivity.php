ef57bf60a9e9853f9ba35330370c7fc0d539e209
SuppanatThreerattananukul <46100073+SuppanatThreerattananukul@users.noreply.github.com>
2019-12-01 14:50:07 +0700

    Update AddActivity.php

--- include/AddActivity.php
+++ include/AddActivity.php
@@ -1,12 +1,27 @@
 <?php
     include("ConnectDB.php");
-    $result = array();
-    mysqli_set_charset($con,"utf8");
-    $sql = "select * from activity,user WHERE  activity.id_host = user.id";
+    // Getting the received JSON into $json variable.
+    $json = file_get_contents('php://input');
+    
+    // decoding the received JSON and store into $obj variable.
+    $obj = json_decode($json,true);
+
+    // $id_host = $obj[''];
+    $date_start = $obj['datetimes'];
+    $title = $obj['title'];
+    $description = $obj['description'];
+    $number_people = $obj['number_people'];
+    $min_age = $obj['minage'];
+    $max_age = $obj['maxage'];
+    $location_name = $obj['location'];
+    $type = $obj['tag'];
+    $gender = $obj['gender'];
+    $sql = "Insert into activity(id_host,date_start,title,description,number_people,min_age,max_age,location_name,type,gender) values (1,'".$date_start."','".$title."','".$description."','".$number_people."','".$min_age."','".$max_age."','".$location_name."','".$type."','".$gender."')";
     $query = mysqli_query($con,$sql);    
-    while($row = mysqli_fetch_array($query,MYSQLI_ASSOC))
-    {
-        array_push($result,$row);
+    if ($con->query($sql) === TRUE) {
+        $result = "New record created successfully";
+    } else {
+        $result = "Error: " . $sql . "<br>" . $query . "<br>" .$conn->error;
     }
     header('Content-type: application/json');
     header("content-type:text/javascript;charset=utf-8");	
